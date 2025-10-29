0bbe2a15306fa26098a6aa4f24a5a490ed4b229e
Erardo Aldana Pessoa <eap59@alu.ua.es>
2025-10-29 16:06:57 +0100

    Modificado el main con nuevos cambios en gameplaystate

--- src/states/GameplayState.hpp
+++ src/states/GameplayState.hpp
@@ -2,24 +2,11 @@
 #include "../core/GameState.hpp"
 #include "../core/StateMachine.hpp"
 #include "GameOverState.hpp"
+#include "../entities/Player.hpp"
+#include "../entities/Platform.hpp"
+#include "../physics/Physics.hpp"
 #include <vector>
 #include <raylib.h>
-
-
-struct Player {
-    Rectangle rect;
-    Vector2 velocity;
-    Color color;
-    bool isGrounded;
-    int leftKey;
-    int rightKey;
-    int jumpKey;
-};
-
-struct Platform {
-    Rectangle rect;
-    Color color;
-};
 
 class GameplayState : public GameState {
 public:
@@ -34,11 +21,30 @@ public:
     void resume() override {}
 
 private:
-    Player player1;
-    Player player2;
+    // Entidades del juego
+    std::vector<Player> players;
     std::vector<Platform> platforms;
-    Rectangle exitZone;
-    bool isExitVisible, isExitMoved; // parte del nivel troll del hito 1
+    ExitZone exitZone;
+    
+    // Motor de físicas
+    PhysicsEngine physicsEngine;
+    
+    // Estado del nivel
     bool levelCompleted;
     bool isGameOver;
+    
+    // Nivel troll (Hito 1) - mantener la funcionalidad original
+    bool isExitVisible;
+    bool isExitMoved;
+    
+    // Constantes de pantalla
+    static constexpr int SCREEN_WIDTH = 1280;
+    static constexpr int SCREEN_HEIGHT = 720;
+    
+    // Métodos auxiliares
+    void setupPlayers();
+    void setupPlatforms();
+    void checkVictoryCondition();
+    void checkDefeatCondition();
+    void handleTrollMechanic();  // Para el nivel troll del Hito 1
 };
