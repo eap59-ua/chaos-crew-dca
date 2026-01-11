617c211bb36c9980b787a27ee574ab7371c0136b
eap59-ua <eap59@alu.ua.es>
2026-01-11 11:13:34 +0100

    feat(input): add gamepad support to InputSystem
    - Read gamepad left stick X-axis with deadzone filtering
    - Support D-pad as alternative to analog stick
    - Map A/Cross button to jump action
    - Handle gamepad disconnection gracefully
    - Maintain existing keyboard input functionality
    
    Players can now control their character using:
    - Analog stick or D-pad for movement
    - A/Cross button for jumping

--- src/systems/InputSystem.cpp
+++ src/systems/InputSystem.cpp
@@ -1,7 +1,9 @@
 #include <raylib.h>
+#include <cmath>
 #include "../entt/entt.hpp"
 #include "../components/Player.hpp"
 #include "../components/Mandos.hpp"
+#include "../utils/Logger.hpp"
 
 void InputSystem(entt::registry& registry) {
     auto view = registry.view<Player, Mandos>();
@@ -10,8 +12,45 @@ void InputSystem(entt::registry& registry) {
         auto& player = view.get<Player>(entity);
         auto& mando = view.get<Mandos>(entity);
 
-        player.left = IsKeyDown(mando.left);
-        player.right = IsKeyDown(mando.right);
-        player.jump = IsKeyPressed(mando.jump);
+        if (mando.type == InputType::KEYBOARD) {
+            // Keyboard input
+            player.left = IsKeyDown(mando.left);
+            player.right = IsKeyDown(mando.right);
+            player.jump = IsKeyPressed(mando.jump);
+        }
+        else if (mando.type == InputType::GAMEPAD) {
+            // Gamepad input
+            int gamepad = mando.gamepadIndex;
+
+            if (IsGamepadAvailable(gamepad)) {
+                // Left stick horizontal axis
+                float axisX = GetGamepadAxisMovement(gamepad, GAMEPAD_AXIS_LEFT_X);
+
+                // Apply deadzone
+                if (std::fabs(axisX) < mando.deadzone) {
+                    axisX = 0.0f;
+                }
+
+                // Convert analog to digital
+                player.left = (axisX < -0.3f);
+                player.right = (axisX > 0.3f);
+
+                // D-pad support as alternative
+                if (IsGamepadButtonDown(gamepad, GAMEPAD_BUTTON_LEFT_FACE_LEFT)) {
+                    player.left = true;
+                }
+                if (IsGamepadButtonDown(gamepad, GAMEPAD_BUTTON_LEFT_FACE_RIGHT)) {
+                    player.right = true;
+                }
+
+                // Jump button (A/Cross button)
+                player.jump = IsGamepadButtonPressed(gamepad, GAMEPAD_BUTTON_RIGHT_FACE_DOWN);
+            } else {
+                // Gamepad disconnected, reset inputs
+                player.left = false;
+                player.right = false;
+                player.jump = false;
+            }
+        }
     }
 }
